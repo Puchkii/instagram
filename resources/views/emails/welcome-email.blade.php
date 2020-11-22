dacef05993e77d24b5dc7aa7fe3d2afc0e585724
puchkii <kevinblank72@gmail.com>
2020-11-23 00:50:31 +0100

    emails voor het welkom heten van de users word nu ook vericht ;)

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welkom bij instagram
+
+Dit is een community voor het delen van momenten en we zijn echt blij dat je ons hebt gekozen! (zoek een leven lol)
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
